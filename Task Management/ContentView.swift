58c0c3e96fd9ea2f3ee2adce5cadb19a8cf86f83
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-08-02 21:41:26 +0530

    Initial Commit

---
+++ Task Management/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task Management
+//
+//  Created by Mayur Vaity on 02/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Task Management/Task_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagementApp.swift
+//  Task Management
+//
+//  Created by Mayur Vaity on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
